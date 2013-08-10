4470b5423a853870acc790c0f52dada16de56d82
Jad Osseiran <jad6@icloud.com>
2013-08-10 15:50:52 +0800

    Initial Commit

---
+++ DOMATI/DOMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DOMAppDelegate.h
+//  DOMATI
+//
+//  Created by Jad Osseiran on 10/08/13.
+//  Copyright (c) 2013 Jad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DOMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DOMATITests/DOMATITests.m
@@ -0,0 +1,34 @@
+//
+//  DOMATITests.m
+//  DOMATITests
+//
+//  Created by Jad Osseiran on 10/08/13.
+//  Copyright (c) 2013 Jad. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DOMATITests : XCTestCase
+
+@end
+
+@implementation DOMATITests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
